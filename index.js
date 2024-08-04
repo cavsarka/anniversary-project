77a2e5274d4c16f75aada43ae9bce8c6c2bcc6db
cavsarka <chinmay.avsarkar@gmail.com>
2024-08-03 22:39:14 -0400

    Added list parsing.

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const messages = ["This shit cool and it work, This shit cool and it work, This shit cool and it work", "it's eefoc and it's funny, it's eefoc and it's funny, it's eefoc and it's funny"]
 
-    let n = 0;
-
     jar.addEventListener('click', function() {
         // Add vibration class to jar
         jar.classList.add('vibrate');
 
-        k = n%2; 
+
+        function getRandomInt(min, max) {
+            const minCeiled = Math.ceil(min);
+            const maxFloored = Math.floor(max);
+            return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+          }
+
+        k = getRandomInt(0, messages.length-1)
+
         // Change text
         text.innerHTML = messages[k];
         text.style.fontSize = "45px";
-        
-        n += 1;
 
         setTimeout(() => {
             jar.classList.remove('vibrate');
